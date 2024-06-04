e567c49cb67dab4288910ae6eec2fd86dcdfc096
leosequani <156928045+leosequani@users.noreply.github.com>
2024-06-04 01:29:51 +0000

    tesst

--- netlify/redisClient.js
+++ netlify/redisClient.js
@@ -1,8 +1,8 @@
 const Redis = require('ioredis');
 
 const redisConfig = new Redis({
-  host: '127.0.0.1',
-  port: '6379',
+  host: 'redis-17944.c8.us-east-1-3.ec2.redns.redis-cloud.com',
+  port: '17944',
   username: "default",
   password: 'tHJvbDYGX5dL2wFrcUH3XblaQgso7Hkp'
 });
