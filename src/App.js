0f4b7f451e1c30225edc0ce2441ae7b57b42ed34
leosequani <156928045+leosequani@users.noreply.github.com>
2024-06-04 02:17:18 +0000

    test

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App() {
     } else {
       let today = new Date();
       let nextweek = new Date(today.getFullYear(), today.getMonth(), today.getDate()+8).toISOString().substring(0, 19) + 'Z';
-      const url = 'https://' + process.env.REACT_APP_AWS_API_ID + '.execute-api.' + process.env.REACT_APP_AWS_API_REGION + '.amazonaws.com/default/game-data-fetch?sport=' + sport + '&commenceTimeTo=' + nextweek;
+      const url = 'https://uq9ndc524g.execute-api.us-east-1.amazonaws.com/default/game-data-fetch?sport=' + sport + '&commenceTimeTo=' + nextweek;
       const playerData = await fetch(url, {
         method: 'GET'
       });
--- src/Components/DataContext.js
+++ src/Components/DataContext.js
@@ -26,7 +26,7 @@ export const DataProvider = (event) => {
       else odds = hockey_data;
     }
     else {
-      const url = 'https://' + process.env.REACT_APP_AWS_API_ID + '.execute-api.' + process.env.REACT_APP_AWS_API_REGION + '.amazonaws.com/default/player-data-fetch?sport=' + event.sport + '&game_id=' + event.game_id + '&specMarkets=' + specMarketsForSport;
+      const url = 'https://uq9ndc524g.execute-api.us-east-1.amazonaws.com/default/player-data-fetch?sport=' + event.sport + '&game_id=' + event.game_id + '&specMarkets=' + specMarketsForSport;
       const playerData = await fetch(url, {
         method: 'GET'
       });
